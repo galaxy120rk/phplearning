71f3d020265d75cc3fef9215c711e0d1451dadfc
Raju Ahmed <36912403+galaxy120rk@users.noreply.github.com>
2019-07-18 15:27:42 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Error Handler tutorial!</title>
+</head>
+<body>
+	<h2>Signup</h2>
+	<form action="includes/signup.inc.php" method="POST">
+		<input type="text" name="first" placeholder="Firstname"><br>
+		<input type="text" name="last" placeholder="Lastname"><br>
+		<input type="text" name="email" placeholder="E-mail"><br>
+		<input type="text" name="uid" placeholder="Username"><br>
+		<input type="password" name="pwd" placeholder="Password"><br>
+		<button type="submit" name="submit">Sign Up</button>
+	</form>
+</body>
+</html>
